aaed148935e0dcc0a6ce9cfe5b59f16eb889552e
zs <bjzhush@gmail.com>
2017-04-24 21:30:19 +0800

    add 2.cpp

---
+++ cppcollegecourse/chapter4/2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 1;
+    x = x+1;
+    cout << "x now is " << x << endl;
+    x++;
+    cout << "x now is " << x << endl;
+    ++x;
+    cout << "x now is " << x << endl;
+    x+=1;
+    cout << "x now is " << x << endl;
+    x = x+1;
+    cout << "x now is " << x << endl;
+    return 0;
+}
