d0d9562f9a00ae84d434c38c6c6ae54ad755863b
zs <bjzhush@gmail.com>
2017-04-24 21:37:18 +0800

    add 3.cpp

---
+++ cppcollegecourse/chapter4/3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 1;
+    cout << "x now is " << x << endl;
+    int y = 1;
+    cout << "y now is " << y << endl;
+    int z = x + y;
+    cout << "z now is " << z << endl;
+    x++;
+    cout << "x now is " << x << endl;
+    return 0;
+}
